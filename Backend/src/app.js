9b2b3d27f7a742b76b1fc2dc79fcef45f79c8823
nt_56 <nagutirth2121@gmail.com>
2024-12-06 18:31:39 +0530

    implemented http methods

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Home page of dev-tinder");
+//this will only handle GET call to the /user
+app.get("/user", (req, res) => {
+  res.send({ fistName: "nagabhushan", lastName: "tirth" });
 });
 
+app.post("/user", (req, res) => {
+  //saving the data
+  res.send("data saved to DB successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("data updated from DB successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("data deleted from DB successfully");
+});
+
+//this will match all the http methods api calls to /test
 app.use("/test", (req, res) => {
-  res.send("Testing the dev-tinder server");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello from the dev-tinder server");
+  res.send("Testing the dev-tinder server-1");
 });
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000...");
 });
+
+//note-order of writing the routes matter a lot
