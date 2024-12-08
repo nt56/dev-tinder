ac41e5ca7ae109b648c05155e86eb8fc4df68847
nt_56 <nagutirth2121@gmail.com>
2024-12-08 09:11:10 +0530

    multiple route handlesrs implemented

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,28 +1,38 @@
 const express = require("express");
 const app = express();
 
-//this will only handle GET call to the /user
-app.get("/user", (req, res) => {
-  res.send({ fistName: "nagabhushan", lastName: "tirth" });
-});
-
-app.post("/user", (req, res) => {
-  //saving the data
-  res.send("data saved to DB successfully");
-});
-
-app.patch("/user", (req, res) => {
-  res.send("data updated from DB successfully");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("data deleted from DB successfully");
-});
-
-//this will match all the http methods api calls to /test
-app.use("/test", (req, res) => {
-  res.send("Testing the dev-tinder server-1");
-});
+//multiple route handlers (below code snippet throes error after executing all the routes because of how js works bts)
+app.use("/user", [
+  (req, res, next) => {
+    console.log("Handling the route user - 1");
+    next();
+    res.send("sending response of user - 1");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user - 2");
+    next();
+    res.send("sending response of user - 2");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user - 3");
+    next();
+    res.send("sending response of user - 3");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user - 4");
+    next();
+    res.send("sending response of user - 4");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user - 5");
+    next();
+    res.send("sending response of user - 5");
+  },
+  (req, res) => {
+    console.log("Handling the route user - 6");
+    res.send("sending response of user - 6");
+  },
+]);
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000...");
