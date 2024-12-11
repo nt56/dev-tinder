f7a76efda08fb1bf3a1952d46786f993a43d9b4b
nt_56 <nagutirth2121@gmail.com>
2024-12-11 10:04:21 +0530

    user post api implemented

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+//post API call for creating user
+app.post("/signup", async (req, res) => {
+  //creting a dummy user
+  const userObj = {
+    firstName: "Roronoa",
+    lastName: "Zoro",
+    emailId: "zoro@gmail.com",
+    password: "zoro@2002",
+    age: 25,
+    gender: "male",
+  };
+  //creting a new instance of user model
+  const user = new User(userObj);
+
+  //hnadling the wrror usinf try & catch
+  try {
+    await user.save(); //saving the data into DB
+    res.send("User added to DB successfully....!"); //sending back the response
+  } catch (err) {
+    res.status(400).send("Error Adding the user....!" + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
