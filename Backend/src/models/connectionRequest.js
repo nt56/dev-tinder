6ad2799307826c5de7fec938739aad810516b685
nt_56 <nagutirth2121@gmail.com>
2024-12-27 11:52:43 +0530

    connectionRequest API && connectionRequest schema && index

---
+++ Backend/src/models/connectionRequest.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepeted", "rejected"],
+        methods: `{VALUE} is incorrect status type`,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+//creating index
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+//toUser and fromUser cannot be same - it is handles in connectionRequest Schema
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("connot send connection request to yourself...!");
+  }
+  next();
+});
+
+const connectionRequestModel = new mongoose.model(
+  "connectionRequestModel",
+  connectionRequestSchema
+);
+module.exports = { connectionRequestModel };
--- Backend/src/routes/request.js
+++ Backend/src/routes/request.js
@@ -1,10 +1,58 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const User = require("../models/user");
+const { connectionRequestModel } = require("../models/connectionRequest");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  res.send(user.firstName + " sent the connection request...!");
-});
+requestRouter.post(
+  "/request/send/:status/:userId",
+  userAuth,
+  async (req, res) => {
+    try {
+      //fetching toUserId, fromUserId, status
+      const fromUserId = req.user._id;
+      const toUserId = req.params.userId;
+      const status = req.params.status;
+
+      //validation - only allowed status
+      const isStatusAllowed = ["ignored", "interested"];
+      if (!isStatusAllowed.includes(status)) {
+        throw new Error("Invalid status type " + status);
+      }
+
+      //checking toUser is valid or not
+      const isValidToUser = await User.findById(toUserId);
+      if (!isValidToUser) {
+        throw new Error("user not found");
+      }
+
+      //check if there is existing connected user in DB or another user sending connection request to existing user
+      const isExistingConnectionnectedUser =
+        await connectionRequestModel.findOne({
+          $or: [
+            { fromUserId, toUserId },
+            { fromUserId: toUserId, toUserId: fromUserId },
+          ],
+        });
+      if (isExistingConnectionnectedUser) {
+        throw new Error("Connection request already exist..!");
+      }
+
+      //save the connection request and response back to the user
+      const connectionRequest = new connectionRequestModel({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      await connectionRequest.save();
+
+      res.send(
+        `${req.user.firstName} your connection request sent to ${isValidToUser.firstName}`
+      );
+    } catch (err) {
+      res.status(400).send("Error : " + err.message);
+    }
+  }
+);
 
 module.exports = requestRouter;
