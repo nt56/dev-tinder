4bdca423c57f6757bcb5c3edd8cbb41a52aee816
nt_56 <nagutirth2121@gmail.com>
2025-01-03 11:01:36 +0530

    security check implemented

--- Frontend/src/components/Body.jsx
+++ Frontend/src/components/Body.jsx
@@ -1,8 +1,40 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const userData = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      //logic for fetching user when token is exist in browser
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  //when component renders then function executes
+  useEffect(() => {
+    //no need to call api again and again
+    if (!userData) {
+      fetchUser();
+    }
+  });
+
   return (
     <div>
       <NavBar />
