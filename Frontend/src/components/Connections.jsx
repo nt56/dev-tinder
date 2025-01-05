51150c68b685b5fa312f6d485a0e8dabc7a38a01
nt_56 <nagutirth2121@gmail.com>
2025-01-05 11:10:39 +0530

    connection page done

--- Frontend/src/components/Connections.jsx
+++ Frontend/src/components/Connections.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addConnections } from "../utils/connectionSlice";
 
 const Connections = () => {
-  return <div>Connections</div>;
+  const connections = useSelector((store) => store.connection);
+  const dispatch = useDispatch();
+
+  const fetchConnections = async () => {
+    try {
+      if (connections) return;
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials: true,
+      });
+      dispatch(addConnections(res.data));
+    } catch (err) {
+      toast.error(err.response.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchConnections();
+  }, []);
+
+  if (!connections) return;
+
+  if (connections.length === 0)
+    return (
+      <h1 className="text-bold text-white text-3xl">
+        Connections Not Found...!
+      </h1>
+    );
+
+  return (
+    connections && (
+      <div className="text-center my-10">
+        <h1 className="text-bold text-white text-3xl">Connections</h1>
+        {connections.map((connection) => {
+          const { _id, firstName, lastName, age, gender, photoUrl, about } =
+            connection;
+
+          return (
+            <div
+              key={_id}
+              className=" flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
+            >
+              <div>
+                <img
+                  src={photoUrl}
+                  className="w-20 h-20 rounded-full object-cover"
+                  alt="user-photo"
+                />
+              </div>
+              <div className="text-left mx-4 ">
+                <h2 className="font-bold text-xl">
+                  {firstName + " " + lastName}
+                </h2>
+                {age && gender && <p>{age + ", " + gender}</p>}
+                <p>{about}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    )
+  );
 };
 
 export default Connections;
