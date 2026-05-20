383b700c566e98fda547285a73f048a7487616af
Nagabhushan Tirth <nagutirth2121@gmail.com>
2026-05-20 13:29:29 +0530

    fix: update CORS origin and restore dynamic BASE_URL assignment for local development

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,10 +8,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://dev-tinder-6r3c.onrender.com/api",
-    ],
+    origin: ["http://localhost:5173", "https://dev-tinder-inky.vercel.app"],
     credentials: true,
   }),
 );
